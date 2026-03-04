5e4ec5aada899aa24175478fa718daf92fc73c63
Jeet Parikh <jeetparikh06@gmail.com>
2026-03-04 15:09:20 -0500

    add vercel url to CORS

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://yucg-analytics-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
